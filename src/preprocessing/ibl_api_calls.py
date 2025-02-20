ed2f29b28337f06c9fa600ecfa82b6a988433c4b
Maria Kesa <maria.kesa@gmail.com>
2025-02-20 21:07:02 +0200

    making api calls explicit

--- src/preprocessing/ibl_api_calls.py
+++ src/preprocessing/ibl_api_calls.py
@@ -1,25 +1,56 @@
-class ProbeLocationLoader:
+from abc import ABC, abstractmethod
+from one.api import ONE
+
+class IBLSingleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(IBLSingleton, cls).__new__(cls)
+            ONE.setup(base_url='https://openalyx.internationalbrainlab.org', silent=True)
+            cls._instance.one= ONE(password='international')
+        return cls._instance
+
+class IBLApiLoader(ABC):
+    @abstractmethod
     def load(self):
-        return "Probe locations loaded"
+        pass
+    def get_eids(self, one):
+        eids = one.search(dataset='channels.mlapdv')
+        return eids
+
+class ProbeLocationLoader(IBLApiLoader):
+    def __init__(self, API_singleton):
+        self.ibl_api = API_singleton
     
-class SpikeDataLoader:
+    def load(self):
+        eids=self.get_eids(self.ibl_api)
+    
+class SpikeDataLoader(IBLApiLoader):
+    def __init__(self, API_singleton):
+        self.ibl_api = API_singleton
+
     def load(self):
         return "Spike data loaded"
     
-class BehaviorDataLoader:
+class BehaviorDataLoader(IBLApiLoader):
+    def __init__(self, API_singleton):
+        self.ibl_api = API_singleton
+        
     def load(self):
         return "Behavior data loaded"
 
 class DataLoaderFactory:
     @staticmethod
-    def get_loader(dataset_type):
+    def get_loader(dataset_type, ibl_api):
         loaders = {
-            "probe_locations": ProbeLocationLoader(),
-            "spike_data": SpikeDataLoader(),
-            "behavior": BehaviorDataLoader()
+            "probe_locations": ProbeLocationLoader(ibl_api),
+            "spike_data": SpikeDataLoader(ibl_api),
+            "behavior": BehaviorDataLoader(ibl_api)
         }
         return loaders.get(dataset_type, None)
 
 # Usage
-probe_loader = DataLoaderFactory.get_loader("probe_locations")
+ibl_api = IBLSingleton().one
+probe_loader = DataLoaderFactory.get_loader("probe_locations", ibl_api)
 probe_data = probe_loader.load()
